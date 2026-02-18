5c068f9356f9008187d21bf3009ff435d65a7c23
samy-abderraouf.mehdid@adesso.de <samy-abderraouf.mehdid@adesso.de>
2026-02-18 11:12:38 +0100

    feat(views): add searchable modifier to SearchView

--- helwa-vista/views/Search/SearchView.swift
+++ helwa-vista/views/Search/SearchView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var text: String = ""
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .searchable(text: $text)
     }
 }
 
