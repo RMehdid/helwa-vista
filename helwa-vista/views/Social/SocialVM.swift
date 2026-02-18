dea96c56ac8b77ab9dcc5b83102f8bd368b9e95d
samy-abderraouf.mehdid@adesso.de <samy-abderraouf.mehdid@adesso.de>
2026-02-18 11:56:36 +0100

    feat(social): add user profile navigation logic to SocialVM

--- helwa-vista/views/Social/SocialVM.swift
+++ helwa-vista/views/Social/SocialVM.swift
@@ -5,3 +5,39 @@
 //  Created by Mehdid, Samy Abderraouf on 06.02.26.
 //
 
+import Foundation
+import Combine
+
+extension SocialView {
+    @MainActor
+    class ViewModel: ObservableObject {
+        
+        @Published private(set) var isLiked: Bool = false
+        @Published private(set) var isBookMarked: Bool = false
+        @Published private(set) var isReposted: Bool = false
+        
+        func likeChallenge() -> Void {
+            isLiked.toggle()
+        }
+        
+        func openComments() -> Void {
+            
+        }
+        
+        func bookmarkChallenge() -> Void {
+            isBookMarked.toggle()
+        }
+        
+        func shareChallenge() -> Void {
+            
+        }
+        
+        func repostChallenge() -> Void {
+            isReposted.toggle()
+        }
+        
+        func openUserProfile() -> Void {
+            
+        }
+    }
+}
